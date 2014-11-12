91a96a74821bf7f42be730bef6a33d6bbe617d4e
cholokei <leesl0416@naver.com>
2014-11-12 21:22:18 +0900

    Revert "Set LOCAL_PATH to fix errors"
    This reverts commit c254e438e3cb2e95b344c3ad5b277f9efc26852a.

--- device.mk
+++ device.mk
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-LOCAL_PATH := $(call my-dir)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
